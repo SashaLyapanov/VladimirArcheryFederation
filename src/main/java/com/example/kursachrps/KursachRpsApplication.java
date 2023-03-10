e8c3d4d970b6846079fac463e868ee14dbd92a11
SashaLyapanov <lyapanov_s@mail.ru>
2023-03-10 18:22:44 +0300

    Создание проекта

---
+++ src/main/java/com/example/kursachrps/KursachRpsApplication.java
@@ -0,0 +1,13 @@
+package com.example.kursachrps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KursachRpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KursachRpsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kursachrps/KursachRpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kursachrps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KursachRpsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
