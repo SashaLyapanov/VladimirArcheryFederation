4e3a60c82361c91b4923da5cc9c960bafadbf8bb
SashaLyapanov <99658299+SashaLyapanov@users.noreply.github.com>
2023-04-26 22:00:53 +0300

    Delete src/main/java/com/example/kursachrps/services/impl directory

--- src/main/java/com/example/kursachrps/services/impl/UserDetailsServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.example.kursachrps.services.impl;
-
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
-public interface UserDetailsServiceImpl {
-    UserDetails loadUserByUserName(String username) throws UsernameNotFoundException;
-}
