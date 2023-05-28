0496c269e210d41ac1bbe74bbe73c1d066c64d1a
SashaLyapanov <lyapanov_s@mail.ru>
2023-05-29 02:22:40 +0300

    Тренер сам регается на соревнования

--- src/main/java/com/example/kursachrps/controllers/CoachController.java
+++ src/main/java/com/example/kursachrps/controllers/CoachController.java
@@ -1,13 +1,13 @@
 package com.example.kursachrps.controllers;
 
+import com.example.kursachrps.Models.Application;
+import com.example.kursachrps.dto.ApplicationDTO;
 import com.example.kursachrps.dto.SportsmanDTO;
+import com.example.kursachrps.mapper.ApplicationMapper;
 import com.example.kursachrps.mapper.SportsmanMapper;
 import com.example.kursachrps.service.CoachService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,11 +17,13 @@ public class CoachController {
 
     private final CoachService coachService;
     private final SportsmanMapper sportsmanMapper;
+    private final ApplicationMapper applicationMapper;
 
     @Autowired
-    public CoachController (CoachService coachService, SportsmanMapper sportsmanMapper) {
+    public CoachController (CoachService coachService, SportsmanMapper sportsmanMapper, ApplicationMapper applicationMapper) {
         this.coachService = coachService;
         this.sportsmanMapper = sportsmanMapper;
+        this.applicationMapper = applicationMapper;
     }
 
     /**
@@ -31,6 +33,20 @@ public class CoachController {
     List<SportsmanDTO> getAllMySportsmen(@RequestParam String coachEmail) {
         List<SportsmanDTO> sportsmenDTO = sportsmanMapper.fromSportsmanToSportsmanDTO(coachService.findAllMySportsmen(coachEmail));
         return sportsmenDTO;
+    }
+
+
+    /**
+     * Метод регистрации на соревнования
+     * Логика такая, что тренер сначала выбирает соревнования! Проваливается в них (Это делается в другом методе в GeneralController)
+     * Дальше на странице соревнований будет кнопка зарегистрироваться, при нажатии на которую у нас происходи выполнение данного метода.
+     * Должна генерироваться заявка на соревнования, которая связана вторичными ключами со спортсменом(1 к мн) и с соревнованиями(1 к мн)
+     */
+    @PostMapping("/regInCompetition")
+    public void regInCompetition(@RequestParam int coachId, @RequestParam int competitionId, @RequestBody ApplicationDTO applicationDTO) {
+
+        Application application = applicationMapper.fromApplicationDTO(applicationDTO);
+        coachService.registrateCoach(coachId, competitionId, application);
     }
 
 }
--- src/main/java/com/example/kursachrps/service/CoachService.java
+++ src/main/java/com/example/kursachrps/service/CoachService.java
@@ -1,8 +1,12 @@
 package com.example.kursachrps.service;
 
+import com.example.kursachrps.Models.Application;
 import com.example.kursachrps.Models.Coach;
+import com.example.kursachrps.Models.Competition;
 import com.example.kursachrps.Models.Sportsman;
+import com.example.kursachrps.repositories.ApplicationRepository;
 import com.example.kursachrps.repositories.CoachMainRepository;
+import com.example.kursachrps.repositories.CompetitionRepository;
 import com.example.kursachrps.repositories.RegistrAndAuth.CoachRepository;
 import com.example.kursachrps.repositories.SportsmanMainRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,14 +22,20 @@ public class CoachService {
     private CoachMainRepository coachMainRepository;
     private CoachRepository coachRepository;
     private SportsmanMainRepository sportsmanMainRepository;
+    private CompetitionRepository competitionRepository;
+    private ApplicationRepository applicationRepository;
 
     @Autowired
     public CoachService(CoachMainRepository coachMainRepository,
                         CoachRepository coachRepository,
-                        SportsmanMainRepository sportsmanMainRepository) {
+                        SportsmanMainRepository sportsmanMainRepository,
+                        CompetitionRepository competitionRepository,
+                        ApplicationRepository applicationRepository) {
         this.coachMainRepository = coachMainRepository;
         this.coachRepository = coachRepository;
         this.sportsmanMainRepository = sportsmanMainRepository;
+        this.competitionRepository = competitionRepository;
+        this.applicationRepository = applicationRepository;
     }
 
 
@@ -35,4 +45,19 @@ public class CoachService {
         assert coach != null;
         return sportsmanMainRepository.findAllSportsmanByPersonalCoach(coach.getId());
     }
+
+    /**
+     * Метод для регистрации спортсмена на соревнования
+     */
+    @Transactional
+    public void registrateCoach(int coachId, int competitionId, Application application) {
+
+        Competition competition = competitionRepository.findById(competitionId).orElse(null);
+        Coach coach = coachMainRepository.findById(coachId).orElse(null);
+
+        application.setCompetition(competition);
+        application.setCoach(coach);
+        applicationRepository.save(application);
+    }
+
 }
